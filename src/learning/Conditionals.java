3c733db43ab62bb4ce433ab1537ae8da8b4dc1a9
Abhattarai12 <aarambhab@googlemail.com>
2023-09-15 14:04:28 +0100

    new conditionals file

---
+++ src/learning/Conditionals.java
@@ -0,0 +1,32 @@
+package learning;
+
+public class Conditionals {
+    public static void main(String[] args) {
+//        int x = -1, y = 5;
+//        if (x>= 0) {
+//            if ( y <= x) {
+//                System.out.println("y is less than x");
+//            } else {
+//                System.out.println("y is more than x");;
+//            };
+//        } else {
+//            System.out.println("your x value should be greater than or equal to 0");;
+//        };
+//        int z = 7;
+//        if (z == 0) {
+//            System.out.println("x is zero");
+//        } else {
+//            System.out.println("x is not zero");
+//        };
+//        int x = 15, y =24;
+//        if ( x % 2 == 0 && y % 2 == 0) {
+//            System.out.println(" x and y are even numbers ");
+//        } else if (x % 2 == 0 & y % 2 != 0) {
+//            System.out.println(" x is even but y is odd ");
+//        } else if ( x % 2 != 0 & y % 2 == 0){
+//            System.out.println(" y is even but x is odd ");
+//        } else {
+//            System.out.println(" Both numbers are odd");
+//        }
+    };
+};
