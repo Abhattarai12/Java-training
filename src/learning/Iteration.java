9af63fe52d37d2bc9b14e366745f2fa891acfc5e
Abhattarai12 <aarambhab@googlemail.com>
2023-09-15 15:06:59 +0100

    Iteration

---
+++ src/learning/Iteration.java
@@ -0,0 +1,41 @@
+package learning;
+
+public class Iteration {
+    public static void main(String[] args) {
+
+//        for (int i = 0; i < 10; i++) {
+//            if (i == 0) {
+//                System.out.println("You are on the first loop");
+//            } else if (i == 1) {
+//                System.out.println("You are on the second loop");
+//            } else if (i == 2) {
+//                System.out.println("You are on the third loop");
+//            } else {
+//                System.out.println("You are on the " + (i+1) + "th loop");
+//            }
+//            ;
+//        }
+//        ;
+//        int i = 0;
+//            do {
+//                System.out.println(" i =" + i);
+//                i++;
+//            } while (i < 10);
+//            int i = 0;
+//            int j = 10;
+//            do {
+//                System.out.println(" i = "+i);
+//                System.out.println(" j = "+j);
+//                i++;
+//                j--;
+//            } while ( i < j);
+//            int sum = 0;
+//            int i = 0;
+//            do {
+//                sum += i;
+//                i++;
+//                System.out.println("The sum is : "+ sum);
+//            } while (i<10);
+//        };
+};
+
